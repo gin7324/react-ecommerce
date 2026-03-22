47e29cf26258a6175fd84fc1fa76da8e882dfaa8
Gene <genekenryp@gmail.com>
2026-03-22 20:35:50 +0800

    Changes to Footer

--- ecommerce-frontend/src/assets/components/Footer.jsx
+++ ecommerce-frontend/src/assets/components/Footer.jsx
@@ -3,13 +3,13 @@ const Footer = () => {
         <footer className="footer-custom text-center p-4">
             <p className="mb-2">&copy; 2026 zKr. All rights reserved.</p>
             <div>
-                <a href="https://facebook.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-link">
+                <a href="https://facebook.com/genekenry.peralta" target="_blank" rel="noopener noreferrer" className="social-link">
                     <i className="fab fa-facebook-f fa-lg mx-2 social-icon" ></i>
                 </a>
-                <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-link">
+                <a href="https://x.com/cakenryy?s=21&t=ZAgXod7Md8fmNkKfD4wp8Q" target="_blank" rel="noopener noreferrer" className="social-link">
                     <i className="fab fa-twitter fa-lg mx-2 social-icon" ></i>
                 </a>
-                <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-link">
+                <a href="https://www.instagram.com/gk_enry" target="_blank" rel="noopener noreferrer" className="social-link">
                     <i className="fab fa-instagram fa-lg mx-2 social-icon" ></i>
                 </a>
             </div>
