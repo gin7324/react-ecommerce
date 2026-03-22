0c3074af0bf62b16fed10af992dfef11e23181e0
Gene <genekenryp@gmail.com>
2026-03-22 19:58:37 +0800

    Changes to about page

--- ecommerce-frontend/src/assets/pages/About.jsx
+++ ecommerce-frontend/src/assets/pages/About.jsx
@@ -5,8 +5,31 @@ const About = () => {
                 <div className="col-lg-8">
                     <h2>About Our Store</h2>
                     <p>
-                        We provide high quality products with affordable prices.
-                        This website is built using React, vite, Bootstrap, and FontAwesome.
+                        We are committed to delivering high-quality products 
+                        at prices that remain accessible to everyone. 
+                        Our mission is to combine durability, style, and functionality so that customers can 
+                        enjoy premium items without the premium cost. 
+                        Every product we offer is carefully selected and crafted to meet the highest standards, 
+                        ensuring that you get the best value for your money.
+                    </p>
+                    <h2>Technology Behind The Website</h2>
+                    <p>
+                        This website is built using modern web technologies to provide a fast, 
+                        responsive, and user-friendly experience:
+                    </p>
+                    <h2>Our Promis</h2>
+                    <p>
+                        We believe shopping should be simple, enjoyable, and affordable. That’s why we focus on:
+                        <ul>
+                            <li><strong>Quality:</strong> We source products that are built to last, ensuring you get the best value for your money.</li>
+                            <li><strong>Affordability:</strong> We strive to keep our prices competitive without compromising on quality.</li>
+                            <li><strong>Customer Satisfaction:</strong> Your satisfaction is our top priority. We are here to help with any questions or concerns you may have.</li>
+                        </ul>
+                    </p>
+                    <h2>Contact Us</h2>
+                    <p>
+                        If you have any questions, feedback, or need assistance, please don’t hesitate to contact us. 
+                        We are here to help and ensure you have the best shopping experience possible.
                     </p>
                 </div>
             </div>
