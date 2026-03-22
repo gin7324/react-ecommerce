6cd3ef72c44f57e58e895e739a83284e32c1046f
Gene <genekenryp@gmail.com>
2026-03-22 19:07:38 +0800

    Update Header component

--- ecommerce-frontend/src/assets/components/Header.jsx
+++ ecommerce-frontend/src/assets/components/Header.jsx
@@ -1,11 +1,7 @@
 const Header = () => {
     return (
         <header className="bg-dark text-white p-3 text-center custom-header">
-<<<<<<< HEAD
             <h1 className="store-title">ZKRA STORE</h1>
-=======
-            <h1 className="store-title">ZAKERA STORE</h1>
->>>>>>> cd602bb (Update frontend to use backend API endpoints)
             <p className="store-subtitle">Find the best products here!</p>
         </header>
     );
