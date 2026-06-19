8dd430f8a48403e1b258631b9db0aea58974f36c
Gene <genekenryp@gmail.com>
2026-06-19 15:09:09 +0800

    final check

--- ecommerce-frontend/src/main.jsx
+++ ecommerce-frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './overrides_readability.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
